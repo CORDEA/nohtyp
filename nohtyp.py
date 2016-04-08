cbb3b8f507eafd18d394532f730f46b1fe94bd3f
Yoshihiro Tanaka <contact@cordea.jp>
2016-04-08 13:26:01 +0900

    yp.python dedda

---
+++ nohtyp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding:utf-8
+#
+# Copyright 2016 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__Author__ =  "Yoshihiro Tanaka <contact@cordea.jp>"
+__date__   =  "2016-04-08"
+
+import sys
+
+src = ""
+with open(sys.argv[1]) as f:
+    for line in f.readlines():
+        line = line[::-1]
+        src += line
+
+mod = compile(src, '<string>', 'exec')
+exec(mod)
